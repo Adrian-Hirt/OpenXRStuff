37ad669ee16522f6454ea7db0c10c68ddd378eaf
Adrian Hirt <aedu.hirt@gmail.com>
2020-05-29 14:22:48 +0200

    Init Repo

---
+++ src/BasicXRCube/source.cpp
@@ -0,0 +1,9 @@
+#pragma comment(lib,"D3D11.lib")
+
+#include <d3d11.h>
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
+	return 0;
+};
